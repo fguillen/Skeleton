ba9871ad253a7362327178013658d6b608235b46
Fernando Guillen <fguillen.mail@gmail.com>
2013-03-12 14:49:08 +0100

    database.yml examples

--- etc/osx_init.rb
+++ etc/osx_init.rb
@@ -1,12 +1,24 @@
 #!/usr/bin/env ruby
 
-if ARGV.length != 1
-  puts "Try osx_init.rg <NewProjectName>"
+if ENV["PROJECT_NAME"].nil?
+  puts "Try $ PROJECT_NAME=<NewProjectName> osx_init.rb"
   exit 1
 end
 
-name = ARGV.first
+
+name = ENV["PROJECT_NAME"]
 puts "Initializing project '#{name}'"
+
+# From: http://stackoverflow.com/a/1509939/316700
+class String
+  def underscore
+    self.gsub(/::/, '/').
+    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    tr("-", "_").
+    downcase
+  end
+end
 
 def shell(command)
   puts "Executing: #{command}"
@@ -16,5 +28,5 @@ end
 
 shell "git clone --depth=1 git@github.com:fguillen/Skeleton.git #{name}"
 shell "rm -rf #{name}/.git"
-shell "find ./#{name}/ -type f | xargs grep 'skeleton' | xargs sed -i '' -e 's/skeleton/#{name.underscore}/'"
-shell "find ./#{name}/ -type f | xargs grep 'Skeleton' | xargs sed -i '' -e 's/Skeleton/#{name}/'"
+shell "find ./NewProject/ -type f ! -name '*_init.rb' | xargs grep 'skeleton' | cut -f1 -d':' | xargs sed -i '' -e 's/skeleton/#{name.underscore}/'"
+shell "find ./NewProject/ -type f ! -name '*_init.rb' | xargs grep 'Skeleton' | cut -f1 -d':' | xargs sed -i '' -e 's/Skeleton/#{name}/'"
