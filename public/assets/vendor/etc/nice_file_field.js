e0a1a89188d4d5ab25322365cdc85cc453dca95c
Fernando Guillen <fguillen.mail@gmail.com>
2014-07-30 16:49:17 +0200

    Updating nice_file_field

--- public/assets/vendor/etc/nice_file_field.js
+++ public/assets/vendor/etc/nice_file_field.js
@@ -1,4 +1,6 @@
 // Based in: http://duckranger.com/2012/06/pretty-file-input-field-in-bootstrap/
+// Version: 0.0.3
+// Compatibility with: Bootstrap 3.2.0 and jQuery 2.1.1
 // Use:
 //     <input class="nice_file_field" type="file" data-label="Choose Document">
 //     <script> $(".nice_file_field").niceFileField(); </script>
@@ -10,12 +12,22 @@
       var label = file_field.attr("data-label") || "Choose File";
 
       file_field.css({"display": "none"});
-      file_field.after("<div class=\"nice_file_field input-append\"><input class=\"input span4\" type=\"text\"><a class=\"btn\">" + label + "</a></div>");
 
-      var nice_file_field = file_field.next(".nice_file_field");
-      nice_file_field.find("a").click( function(){ file_field.click() } );
+      nice_file_block_text  = '<div class="input-group nice_file_block">';
+      nice_file_block_text += '  <input type="text" class="form-control">';
+      nice_file_block_text += '  <span class="input-group-btn">';
+      nice_file_block_text += '   <button class="btn btn-default nice_file_field_button" type="button">' + label + '</button>';
+      nice_file_block_text += '  </span>';
+      nice_file_block_text += '</div>';
+
+      file_field.after(nice_file_block_text);
+
+      var nice_file_field_button = file_field.parent().find(".nice_file_field_button");
+      var nice_file_block_element = file_field.parent().find(".nice_file_block");
+
+      nice_file_field_button.on("click", function(){ console.log("click"); file_field.click() } );
       file_field.change( function(){
-        nice_file_field.find("input").val(file_field.val());
+        nice_file_block_element.find("input").val(file_field.val());
       });
     });
   };
